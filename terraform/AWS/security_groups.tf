36d73b2f8c684662d4cbe7ae31c2d2f269dbd383
Walter Gunasekaran <walter.gunasekaran@gmail.com>
2020-11-12 21:49:30 +0800

    Fixing the conf.toml

--- terraform/AWS/security_groups.tf
+++ terraform/AWS/security_groups.tf
@@ -43,8 +43,8 @@ resource "aws_security_group" "rds_sg" {
 
     ingress {
         protocol        = "tcp"
-        from_port       = 3306
-        to_port         = 3306
+        from_port       = 5432
+        to_port         = 5432
         // cidr_blocks     = ["0.0.0.0/0"]
         security_groups = [aws_security_group.ecs_sg.id]
     }
